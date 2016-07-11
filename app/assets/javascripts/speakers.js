a44b4e41967a74736539d1befc0a6a2370070b11
apwn <thomas.kanze@gmail.com>
2016-07-11 17:07:58 +1000

    merge conflicts

--- app/assets/javascripts/speakers.js
+++ app/assets/javascripts/speakers.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-// $(function(){
-//
-//     // Get the Thomas modal
-//     var modal = document.getElementById('myModal');
-//     // Get the Luke modal
-//     var modal = document.getElementById('myModal');
-//
-//     // Get the button that opens the modal
-//     var btn = document.getElementById("myBtn");
-//
-//     // Get the <span> element that closes the modal
-//     var span = document.getElementsByClassName("close")[0];
-//
-//     // When the user clicks the button, open the modal
-//     btn.onclick = function() {
-//         modal.style.display = "block";
-//     }
-//
-//     // When the user clicks on <span> (x), close the modal
-//     span.onclick = function() {
-//         modal.style.display = "none";
-//     }
-//
-//     // When the user clicks anywhere outside of the modal, close it
-//     window.onclick = function(event) {
-//         if (event.target == modal) {
-//             modal.style.display = "none";
-//         }
-//     }
-// });
-=======
 $(function(){
 
     // Get the Thomas modal
@@ -62,4 +30,3 @@ $(function(){
         }
     }
 });
->>>>>>> fb8a8f92232a88db9b09b986ec09e728e18db348
