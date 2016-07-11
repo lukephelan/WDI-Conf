b1bc2446fb5ca7fbdc490fcef0f5626633e73b93
Thomas Langnickel <Langnickel.thomas@gmail.com>
2016-07-11 14:14:03 +1000

    before merge

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,5 +1,4 @@
-/*global $, location*/
-
+/*global $, location, document*/
 $(function () {
     $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
@@ -13,4 +12,16 @@ $(function () {
             }
         }
     });
+
+    function slider() {
+        if (document.body.scrollTop > 800) //Show the slider after scrolling down 100px
+            $('#slider').stop().animate({"margin-left": '0'});
+        else
+            $('#slider').stop().animate({"margin-left": '-200'}); //200 matches the width of the slider
+    }
+
+    $(window).scroll(function () {
+        slider();
+    });
+
 });
