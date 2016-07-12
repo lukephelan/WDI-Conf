fc160ab1fd77af69afc6dd31ded467d8be9f9a30
Thomas Langnickel <Langnickel.thomas@gmail.com>
2016-07-12 16:20:02 +1000

    color responsive navbar

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -16,14 +16,26 @@ $('a[href*="#"]:not([href="#"])').click(function () {
 
 
     $(document).on("scroll", function () {
-      // console.log("fired");
           var height = $(window).scrollTop();
-          // console.log(height);
+          console.log(height);
 
-        if (height < 587) {
-                  $('.navbar').css('margin-right', '-30%');
-        } else {
-                  $('.navbar').css('margin-right', '2%');
-        }
+        if (height < 580) {$('.navbar').css('margin-right', '-30%')}
+        else {$('.navbar').css('margin-right', '2%')};
+
+        if (height > 2318 && height < 3700 || height > 4273){$("#top").css('color', 'white')}
+        else {$("#top").css('color', '#282CC1')}
+        if (height > 2278 && height < 3660 || height > 4233){$("#conf").css('color', 'white')}
+        else {$("#conf").css('color', '#282CC1')}
+        if (height > 2238 && height < 3620 || height > 4193){$("#time").css('color', 'white')}
+        else {$("#time").css('color', '#282CC1')}
+        if (height > 2198 && height < 3580 || height > 4153){$("#profile").css('color', 'white')}
+        else {$("#profile").css('color', '#282CC1')}
+        if (height > 2158 && height < 3540 || height > 4113){$("#seats").css('color', 'white')}
+        else {$("#seats").css('color', '#282CC1')}
+        if (height > 2118 && height < 3500 || height > 4073){$("#sponsor").css('color', 'white')}
+        else {$("#sponsor").css('color', '#282CC1')}
+        if (height > 2078 && height < 3460 || height > 4033){$("#foot").css('color', 'white')}
+        else {$("#foot").css('color', '#282CC1')}
+
     });
 });
