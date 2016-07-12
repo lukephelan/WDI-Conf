1e0701426c6c323a257bc787bd0f600399462509
Ephrem <eakele@gmail.com>
2016-07-12 16:24:36 +1000

    modification on app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,23 +3,23 @@ $(document).ready(function() {
     var $cart = $('#selected-seats'), //Sitting Area
         $counter = $('#counter'), //Votes
         $total = $('#total'); //Total money
+    var seatstaken = [];
 
     var sc = $('#seat-map').seatCharts({
         map: [ //Seating charts
-            'a[ID,A1]a[ID,A2]a[ID,A3]a[ID,A4]_a[ID,A6]a[ID,A7]a[ID,A8]a[ID,A9]',
-            'a[ID,B1]a[ID,B2]a[ID,B3]a[ID,B4]a[ID,B5]a[ID,B6]a[ID,B7]a[ID,B8]a[ID,B9]',
-            'a[ID,C1]a[ID,C2]a[ID,C3]a[ID,C4]a[ID,C5]a[ID,C6]a[ID,C7]a[ID,C8]a[ID,C9]',
-            'a[ID,D1]a[ID,D2]a[ID,D3]a[ID,D4]a[ID,D5]a[ID,D6]a[ID,D7]a[ID,D8]a[ID,D9]',
-            'a[ID,E1]a[ID,E2]a[ID,E3]a[ID,E4]a[ID,E5]a[ID,E6]a[ID,E7]__',
-            'a[ID,F1]a[ID,F2]a[ID,F3]a[ID,F4]a[ID,F5]a[ID,F6]a[ID,F7]a[ID,F8]a[ID,F9]',
-            'a[ID,G1]a[ID,G2]a[ID,G3]a[ID,G4]a[ID,G5]a[ID,G6]a[ID,G7]a[ID,G8]a[ID,G9]'
+            'a[,A1]a[,A2]a[,A3]a[,A4]a[,A5]a[,A6]a[,A7]a[,A8]a[,A9],a[,A10]',
+            'a[,B1]a[,B2]a[,B3]a[,B4]a[,B5]a[,B6]a[,B7]a[,B8]a[,B9]a[,B10]',
+            'a[,C1]a[,C2]a[,C3]a[,C4]a[,C5]a[,C6]a[,C7]a[,C8]a[,C9]a[,C10]',
+            'a[,D1]a[,D2]a[,D3]a[,D4]a[,D5]a[,D6]a[,D7]a[,D8]a[,D9]a[,D10]',
+            'a[,E1]a[,E2]a[,E3]a[,E4]a[,E5]a[,E6]a[,E7]a[,E8]a[,E9]a[,E10]',
+            'a[,F1]a[,F2]a[,F3]a[,F4]a[,F5]a[,F6]a[,F7]a[,F8]a[,F9]a[,F10]',
+            'a[,G1]a[,G2]a[,G3]a[,G4]a[,G5]a[,G6]a[,G7]a[,G8]a[,G9]a[,G10]'
         ],
 
         // 'a[ID,LABEL]a[ID2,LABEL2]a___a[JUST_ID1]aa',
 
         naming: {
             top: false,
-            // columns: ['A', 'B', 'C', 'D', 'E','F','G','H','I','J'],
             getLabel: function(character, row, column) {
                 return column;
             }
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
                 $counter.text(sc.find('selected').length + 1);
                 $total.text(recalculateTotal(sc) + price);
-
+                seatstaken.push(this.settings.row + " " + this.settings.label);
                 return 'selected';
             } else if (this.status() == 'selected') { //Checked
                 //Update Number
@@ -63,15 +63,18 @@ $(document).ready(function() {
         }
     });
     //sold seat
-    // sc.get(['1_2', '4_4', '4_5', '6_6', '6_7', '8_5', '8_6', '8_7', '8_8', '10_1', '10_2']).status('unavailable');
-
+    sc.get(['1_2', '4_4', '4_5', '6_6', '6_7', '8_5', '8_6', '8_7', '8_8', '10_1', '10_2']).status('unavailable');
 });
 //sum total money
+
 function recalculateTotal(sc) {
     var total = 0;
     sc.find('selected').each(function() {
         total += price;
     });
-
     return total;
 }
+
+$('.checkout-button').on('click', function() {
+    console.log("Click event of the button");
+});
