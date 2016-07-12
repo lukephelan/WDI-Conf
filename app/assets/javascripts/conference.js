ab2c05b9460a78bbeef0be7bdad7052d89c7ac65
apwn <thomas.kanze@gmail.com>
2016-07-12 17:10:25 +1000

    add modal booking

--- app/assets/javascripts/conference.js
+++ app/assets/javascripts/conference.js
@@ -1,7 +1,7 @@
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {zoom: 14});
         var geocoder = new google.maps.Geocoder;
-        geocoder.geocode({'address': "52a Willam Street, Melbourne",}, function(results, status) {
+        geocoder.geocode({'address': "52a William Street, Melbourne, Vic 3000"}, function(results, status) {
           if (status === google.maps.GeocoderStatus.OK) {
             map.setCenter(results[0].geometry.location);
             new google.maps.Marker({
