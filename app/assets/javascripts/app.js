637e2b4cf4da61775bfe70580c210069d07e9996
Ephrem <eakele@gmail.com>
2016-07-12 14:17:24 +1000

    modified styling

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,15 +6,20 @@ $(document).ready(function() {
 
     var sc = $('#seat-map').seatCharts({
         map: [ //Seating charts
-            'aaaa_aaaaa',
-            'aaaa_aaaaa',
-            'aaaaaaaaaa',
-            'aaaaaaaa__',
-            'aaaaaaaaaa',
-            'aaaaaaaaaa'
+            'a[ID,A1]a[ID,A2]a[ID,A3]a[ID,A4]_a[ID,A6]a[ID,A7]a[ID,A8]a[ID,A9]',
+            'a[ID,B1]a[ID,B2]a[ID,B3]a[ID,B4]a[ID,B5]a[ID,B6]a[ID,B7]a[ID,B8]a[ID,B9]',
+            'a[ID,C1]a[ID,C2]a[ID,C3]a[ID,C4]a[ID,C5]a[ID,C6]a[ID,C7]a[ID,C8]a[ID,C9]',
+            'a[ID,D1]a[ID,D2]a[ID,D3]a[ID,D4]a[ID,D5]a[ID,D6]a[ID,D7]a[ID,D8]a[ID,D9]',
+            'a[ID,E1]a[ID,E2]a[ID,E3]a[ID,E4]a[ID,E5]a[ID,E6]a[ID,E7]__',
+            'a[ID,F1]a[ID,F2]a[ID,F3]a[ID,F4]a[ID,F5]a[ID,F6]a[ID,F7]a[ID,F8]a[ID,F9]',
+            'a[ID,G1]a[ID,G2]a[ID,G3]a[ID,G4]a[ID,G5]a[ID,G6]a[ID,G7]a[ID,G8]a[ID,G9]'
         ],
+
+        // 'a[ID,LABEL]a[ID2,LABEL2]a___a[JUST_ID1]aa',
+
         naming: {
             top: false,
+            // columns: ['A', 'B', 'C', 'D', 'E','F','G','H','I','J'],
             getLabel: function(character, row, column) {
                 return column;
             }
