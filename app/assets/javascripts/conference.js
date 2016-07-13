5203a830310d0e3481d37df85de09ecbc860f857
Brad Wynter <brawyn@Brads-MacBook-Air.local>
2016-07-13 16:49:10 +1000

    Map fix

--- app/assets/javascripts/conference.js
+++ app/assets/javascripts/conference.js
@@ -1,12 +1,14 @@
 function initMap(options) {
 		options = $.extend({
-			scrollwheel: true,
+			scrollwheel: false,
+			scaleControl: false,
+			zoomControl: true,
 			navigationControl: true,
-			mapTypeControl: true,
-			scaleControl: true,
-			draggable: true,
+			mapTypeControl: false,
+			draggable: false,
 		}, options);
         var map = new google.maps.Map(document.getElementById('map'), {zoom: 14});
+				map.setOptions(options);
         var geocoder = new google.maps.Geocoder;
         geocoder.geocode({'address': "52a William Street, Melbourne, Vic 3000"}, function(results, status) {
           if (status === google.maps.GeocoderStatus.OK) {
