d63f15a1baecf772175722f12840d53d8f5efffc
Ephrem <eakele@gmail.com>
2016-07-13 16:25:10 +1000

    add some styling

--- app/assets/javascripts/booking-modal.js
+++ app/assets/javascripts/booking-modal.js
@@ -1,10 +1,10 @@
-// $(function(){
-//
-//   $('.checkout-button').click(function(){
-//     console.log('hello');
-//     $('.modal-overlay').show();
-//     $('.Nav-container').hide();
-//   });
-//
-//
-// });
+$(function(){
+
+  $('.checkout-button').click(function(){
+    console.log('hello');
+    $('.modal-overlay').show();
+    $('.Nav-container').hide();
+  });
+
+
+});
