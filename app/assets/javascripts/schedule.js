c905abf9478a1afc5a9aa409e88155fd93fc0ae2
Brad Wynter <brawyn@Brads-MacBook-Air.local>
2016-07-14 16:42:57 +1000

    session modals

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,7 +1,11 @@
-// $(function(){
-//
-//     // When user hovers over the timetable session, display the modal
-//     $('#dropProfile').hover(function() {
-//         $('#dropContent').css("display", "block");
-//     });
-// )};
+$(function(){
+
+    // When user hovers over the timetable session, display the modal
+    $('.dropProfile').hover(function() {
+        $('.dropContent').show()
+												.css("width", "330px");
+    });
+		$('.dropContent').mouseleave(function() {
+        $('.dropContent').css("display", "none");
+    });
+});
