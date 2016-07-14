3455b4c6d1554bd565371e3f867b17a381a935a6
stephenkoo <stephen.koo.yr@gmail.com>
2016-07-14 14:54:09 +1000

    Successfully store booked seats in database.

--- app/controllers/seatings_controller.rb
+++ app/controllers/seatings_controller.rb
@@ -1,46 +1,49 @@
 class SeatingsController < ApplicationController
 
-	def index
-		@seats = Seating.all
-		render json: @seats.to_json
-	end
+	# def index
+	# 	@seats = Seating.all
+	# 	render json: @seats.to_json
+	# end
 
+	Find the right controller (which points to indx page), grab all seats columns from seating tabel, join all into big array, iterate through to grab each seat id, parse that into jQuery for what those are.
+@user = User.alll
 	def create
 		seat = Seating.new
-		seat.seating = params[:seats].split(',')
+		seat.seating = params[:post][:seats].split(',')
 		seat.save
+		redirect_to root
 	end
 
-	def update
-		seat = Seating.find(params[:id].to_i)
-		seat.status = params[:occupied]
-		seat.save
-		render json: seat.to_json
-	end
+	# def update
+	# 	seat = Seating.find(params[:id].to_i)
+	# 	seat.status = params[:occupied]
+	# 	seat.save
+	# 	render json: seat.to_json
+	# end
 
-	def reserve_seats
-		seat_numbers = params[:seating]
-		reserved_seats = []
-		seat_numbers.each do |seat_num|
-			seat = Seating.find_by(seating: seat_num)
-			seat.occupied = true
-			seat.save
-			reserved_seats << seat
-		end
-		render json: reserve_seats.to_json
-	end
+	# def reserve_seats
+	# 	seat_numbers = params[:seating]
+	# 	reserved_seats = []
+	# 	seat_numbers.each do |seat_num|
+	# 		seat = Seating.find_by(seating: seat_num)
+	# 		seat.occupied = true
+	# 		seat.save
+	# 		reserved_seats << seat
+	# 	end
+	# 	render json: reserve_seats.to_json
+	# end
 
-	def unreserve_seats
-		seat_numbers = params[:seating]
-		reserved_seats = []
-		seat_numbers.each do |seat_num|
-			seat = Seating.find_by(seating: seat_num)
-			seat.occupied = false
-			seat.save
-			unreserve_seats << seat
-		end
-		render json: unreserve_seats.to_json
-	end	
+	# def unreserve_seats
+	# 	seat_numbers = params[:seating]
+	# 	reserved_seats = []
+	# 	seat_numbers.each do |seat_num|
+	# 		seat = Seating.find_by(seating: seat_num)
+	# 		seat.occupied = false
+	# 		seat.save
+	# 		unreserve_seats << seat
+	# 	end
+	# 	render json: unreserve_seats.to_json
+	# end	
 
 	def show
 		#insert logic to rule out seats taken
