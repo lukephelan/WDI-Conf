3e646cb476d8ac1c38f8f6e6d1c81e1d17ebad35
Brad Wynter <brawyn@Brads-MacBook-Air.local>
2016-07-15 11:04:14 +1000

    Fixed width session boxes

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,107 +1,108 @@
-$(function(){
+$(function() {
 
     // When user hovers over the timetable session, display the modal
-    $('.dropProfile1').hover(function() {
+		$('.dropProfile1').hover(function() {
         $('.dropContent1').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent1').mouseleave(function() {
+    $('.dropContent1').mouseleave(function() {
         $('.dropContent1').css("display", "none");
     });
 
-		$('.dropProfile2').hover(function() {
+
+    $('.dropProfile2').hover(function() {
         $('.dropContent2').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent2').mouseleave(function() {
+    $('.dropContent2').mouseleave(function() {
         $('.dropContent2').css("display", "none");
     });
 
-		$('.dropProfile3').hover(function() {
+    $('.dropProfile3').hover(function() {
         $('.dropContent3').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent3').mouseleave(function() {
+    $('.dropContent3').mouseleave(function() {
         $('.dropContent3').css("display", "none");
     });
 
-		$('.dropProfile4').hover(function() {
+    $('.dropProfile4').hover(function() {
         $('.dropContent4').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent4').mouseleave(function() {
+    $('.dropContent4').mouseleave(function() {
         $('.dropContent4').css("display", "none");
     });
 
-		$('.dropProfile5').hover(function() {
+    $('.dropProfile5').hover(function() {
         $('.dropContent5').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent5').mouseleave(function() {
+    $('.dropContent5').mouseleave(function() {
         $('.dropContent5').css("display", "none");
     });
 
-		$('.dropProfile6').hover(function() {
+    $('.dropProfile6').hover(function() {
         $('.dropContent6').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent6').mouseleave(function() {
+    $('.dropContent6').mouseleave(function() {
         $('.dropContent6').css("display", "none");
     });
 
-		$('.dropProfile7').hover(function() {
+    $('.dropProfile7').hover(function() {
         $('.dropContent7').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent7').mouseleave(function() {
+    $('.dropContent7').mouseleave(function() {
         $('.dropContent7').css("display", "none");
     });
 
-		$('.dropProfile8').hover(function() {
+    $('.dropProfile8').hover(function() {
         $('.dropContent8').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent8').mouseleave(function() {
+    $('.dropContent8').mouseleave(function() {
         $('.dropContent8').css("display", "none");
     });
 
-		$('.dropProfile9').hover(function() {
+    $('.dropProfile9').hover(function() {
         $('.dropContent9').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent9').mouseleave(function() {
+    $('.dropContent9').mouseleave(function() {
         $('.dropContent9').css("display", "none");
     });
 
-		$('.dropProfile10').hover(function() {
+    $('.dropProfile10').hover(function() {
         $('.dropContent10').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent10').mouseleave(function() {
+    $('.dropContent10').mouseleave(function() {
         $('.dropContent10').css("display", "none");
     });
 
-		$('.dropProfile11').hover(function() {
+    $('.dropProfile11').hover(function() {
         $('.dropContent11').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent11').mouseleave(function() {
+    $('.dropContent11').mouseleave(function() {
         $('.dropContent11').css("display", "none");
     });
 
-		$('.dropProfile12').hover(function() {
+    $('.dropProfile12').hover(function() {
         $('.dropContent12').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent12').mouseleave(function() {
+    $('.dropContent12').mouseleave(function() {
         $('.dropContent12').css("display", "none");
     });
 
-		$('.dropProfile13').hover(function() {
+    $('.dropProfile13').hover(function() {
         $('.dropContent13').show()
-												.css("width", "368px");
+            .css("width", "368px");
     });
-		$('.dropContent13').mouseleave(function() {
+    $('.dropContent13').mouseleave(function() {
         $('.dropContent13').css("display", "none");
     });
 });
